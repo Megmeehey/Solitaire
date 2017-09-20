b9f7f140774839d1f6a053b7f147d53f4aa33905
megmeehey <megmeehey@gmail.com>
2017-09-20 12:12:45 +0300

    Added basic classes, written by Tim Budd.

--- core/src/com/github/megmeehey/Solitaire.java
+++ core/src/com/github/megmeehey/Solitaire.java
@@ -6,14 +6,38 @@ import com.badlogic.gdx.graphics.GL20;
 import com.badlogic.gdx.graphics.Texture;
 import com.badlogic.gdx.graphics.g2d.SpriteBatch;
 
+import java.applet.Applet;
+import java.awt.*;
+
 public class Solitaire extends ApplicationAdapter {
 	SpriteBatch batch;
 	Texture img;
+	static DeckPile deckPile;
+	static DiscardPile discardPile;
+	static TablePile tableau[];
+	static SuitPile suitPile[];
+	static CardPile allPiles[];
 	
 	@Override
 	public void create () {
 		batch = new SpriteBatch();
 		img = new Texture("cardback.png");
+
+		// first allocate the arrays
+		allPiles = new CardPile[13];
+		suitPile = new SuitPile[4];
+		tableau = new TablePile[7];
+		// then fill them in
+		allPiles[0] = deckPile = new DeckPile(335, 5);
+		allPiles[1] = discardPile = new DiscardPile(268, 5);
+		for (int i = 0; i < 4; i++) {
+			allPiles[2 + i] = suitPile[i] =
+					new SuitPile(15 + 60 * i, 5);
+		}
+		for (int i = 0; i < 7; i++) {
+			allPiles[6 + i] = tableau[i] =
+					new TablePile(5 + 55 * i, 80, i + 1);
+		}
 	}
 
 	@Override
@@ -31,3 +55,54 @@ public class Solitaire extends ApplicationAdapter {
 		img.dispose();
 	}
 }
+
+/* package lesson170719.homework;
+
+
+
+public class Solitare extends Applet {
+	static DeckPile deckPile;
+	static DiscardPile discardPile;
+	static TablePile tableau[];
+	static SuitPile suitPile[];
+	static CardPile allPiles[];
+
+	@Override
+	public void init() {
+		// first allocate the arrays
+		allPiles = new CardPile[13];
+		suitPile = new SuitPile[4];
+		tableau = new TablePile[7];
+		// then fill them in
+		allPiles[0] = deckPile = new DeckPile(335, 5);
+		allPiles[1] = discardPile = new DiscardPile(268, 5);
+		for (int i = 0; i < 4; i++) {
+			allPiles[2 + i] = suitPile[i] =
+					new SuitPile(15 + 60 * i, 5);
+		}
+		for (int i = 0; i < 7; i++) {
+			allPiles[6 + i] = tableau[i] =
+					new TablePile(5 + 55 * i, 80, i + 1);
+		}
+	}
+
+	@Override
+	public void paint(Graphics g) {
+		for (int i = 0; i < 13; i++) {
+			allPiles[i].display(g);
+		}
+	}
+
+	@Override
+	public boolean mouseDown(Event evt, int x, int y) {
+		for (int i = 0; i < 13; i++) {
+			if (allPiles[i].includes(x, y)) {
+				allPiles[i].select(x, y);
+				repaint();
+				return true;
+			}
+		}
+		return true;
+	}
+}
+ */
