be8fb9a1c30249e41a98bc2cb237d47e038427f0
megmeehey <megmeehey@gmail.com>
2017-09-21 15:57:57 +0300

    Discard pile quickfix

--- core/src/com/github/megmeehey/DiscardPile.java
+++ core/src/com/github/megmeehey/DiscardPile.java
@@ -1,11 +1,11 @@
 package com.github.megmeehey;
 
-import com.badlogic.gdx.graphics.g2d.SpriteBatch;
+import com.badlogic.gdx.graphics.g2d.TextureAtlas;
 
 class DiscardPile extends CardPile {
 
-    DiscardPile(int x, int y) {
-        super(x, y);
+    DiscardPile(TextureAtlas atlas) {
+        super(atlas);
     }
 
     @Override
@@ -16,40 +16,27 @@ class DiscardPile extends CardPile {
         super.push(aCard);
     }
 
-    @Override
-    public void select(int tx, int ty) {
-        if (isEmpty()) {
-            return;
-        }
-        Card topCard = pop();
-        for (int i = 0; i < 4; i++) {
-            // Checking, if we can put this card in SuitPiles
-            if (Solitaire.suitPile[i].canTake(topCard)) {
-                Solitaire.suitPile[i].push(topCard);
-                return;
-            }
-        }
-        for (int i = 0; i < 7; i++) {
-            // ...if we can't, trying to put this card in TablePiles
-            if (Solitaire.tableau[i].canTake(topCard)) {
-                Solitaire.tableau[i].push(topCard);
-                return;
-            }
-        }
-        // nobody can use it, put it back on our list
-        push(topCard);
-    }
-
-    @Override
-    public void display(SpriteBatch mainBatch) {
-        if (isEmpty()) {
-            mainBatch.draw(Card.EMPTY, x, y);
-        } else {
-            Card card = cards.peek();
-            if (!card.isFaceUp()) {
-                card.flip();
-            }
-            mainBatch.draw(card.getTexture(), x, y);
-        }
-    }
+//    @Override
+//    public void select(int tx, int ty) {
+//        if (isEmpty()) {
+//            return;
+//        }
+//        Card topCard = pop();
+//        for (int i = 0; i < 4; i++) {
+//            // Checking, if we can put this card in SuitPiles
+//            if (Solitaire.suitPile[i].canTake(topCard)) {
+//                Solitaire.suitPile[i].push(topCard);
+//                return;
+//            }
+//        }
+//        for (int i = 0; i < 7; i++) {
+//            // ...if we can't, trying to put this card in TablePiles
+//            if (Solitaire.tableau[i].canTake(topCard)) {
+//                Solitaire.tableau[i].push(topCard);
+//                return;
+//            }
+//        }
+//        // nobody can use it, put it back on our list
+//        push(topCard);
+//    }
 }
