0f0d831b2bb4abf943ed0764e194204f7c311053
megmeehey <megmeehey@gmail.com>
2017-09-21 15:27:31 +0300

    Replaced enums with more suitable versions

--- core/src/com/github/megmeehey/Card.java
+++ core/src/com/github/megmeehey/Card.java
@@ -5,9 +5,25 @@ import com.badlogic.gdx.graphics.Texture;
 import com.badlogic.gdx.graphics.g2d.SpriteBatch;
 
 class Card {
-    public enum Suit { CLUBS, DIAMONDS, HEARTS, SPADES }
-    public enum Rank { ACE, TWO, THREE, FOUR, FIVE, SIX, SEVEN, EIGHT, NINE, TEN, JACK, QUEEN, KING }
-    public enum Color { RED, BLACK }
+    public enum Suit {
+        Club("club", 0), Diamond("diamond", 1), Heart("heart", 2), Spade("spade", 3);
+        public final String name;
+        public final int index;
+
+        private Suit(String name, int index) {
+            this.name = name;
+            this.index = index;
+        }
+    }
+
+    public enum Rank {
+        Ace(1), Two(2), Three(3), Four(4), Five(5), Six(6), Seven(7), Eight(8), Nine(9), Ten(10), Jack(11), Queen(12), King(13);
+        public final int value;
+
+        private Rank(int value) {
+            this.value = value;
+        }
+    }
 
     public static final Texture BACKSIDE = new Texture("cardback.png");
     public static final Texture EMPTY = new Texture("empty.png");
--- core/src/com/github/megmeehey/Solitaire.java
+++ core/src/com/github/megmeehey/Solitaire.java
@@ -2,7 +2,6 @@ package com.github.megmeehey;
 
 import com.badlogic.gdx.ApplicationAdapter;
 import com.badlogic.gdx.Gdx;
-import com.badlogic.gdx.Screen;
 import com.badlogic.gdx.graphics.GL20;
 import com.badlogic.gdx.graphics.Texture;
 import com.badlogic.gdx.graphics.g2d.SpriteBatch;
